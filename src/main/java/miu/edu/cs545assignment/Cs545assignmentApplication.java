1962ab37f7965621c755184d37366037f2b3fc56
Jarkynbyek Japar <18748558+devsteppe9@users.noreply.github.com>
2025-02-25 13:55:50 -0600

    project init

---
+++ src/main/java/miu/edu/cs545assignment/Cs545assignmentApplication.java
@@ -0,0 +1,13 @@
+package miu.edu.cs545assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs545assignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs545assignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/miu/edu/cs545assignment/Cs545assignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.cs545assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs545assignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
